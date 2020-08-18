c32468b5a8f1ded8ed7cd3e814aa83d21300de44
Mayank Raj <AgrMayank@outlook.com>
2020-08-19 00:07:16 +0530

    Update Code

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("I Am Rich"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/4761916/pexels-photo-4761916.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+          ),
+        ),
+        backgroundColor: Colors.grey[900],
       ),
     ),
   );
