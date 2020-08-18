0cb74d269fa2570a2269cf4438aa4998a61019dc
Mayank Raj <AgrMayank@outlook.com>
2020-08-18 23:01:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// The code starts here in main
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World!"),
+      ),
+    ),
+  );
+}
