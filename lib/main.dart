b8a3bd4a3b53b8c8166564f7d601f4f14be70236
Mayank Raj <AgrMayank@outlook.com>
2020-08-20 13:43:17 +0530

    Add Files, Icons

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: Center(
-            child: Text("I Am Rich"),
+            child: Text('I Am Rich'),
           ),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://images.pexels.com/photos/4761916/pexels-photo-4761916.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+            image: AssetImage('Images/diamond.png'),
           ),
         ),
         backgroundColor: Colors.grey[900],
